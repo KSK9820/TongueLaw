2a1c7f588aa1c332c1f7bf8a682cb0370e5dc486
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-10-08 16:30:59 +0900

    feat(init): 프로젝트 초기 설정

---
+++ TongueLaw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TongueLaw
+//
+//  Created by 김수경 on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
