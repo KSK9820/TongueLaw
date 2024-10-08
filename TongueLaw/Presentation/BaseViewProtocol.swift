a35be723c93e2224111710e41a60dad2bd77b6c9
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-10-08 17:55:02 +0900

    feat(Protocol): BaseViewProtocol 생성

---
+++ TongueLaw/Presentation/BaseViewProtocol.swift
@@ -0,0 +1,39 @@
+//
+//  BaseViewProtocol.swift
+//  TongueLaw
+//
+//  Created by 최승범 on 10/8/24.
+//
+
+import Foundation
+
+protocol BaseViewProtocol: AnyObject {
+    
+    func configureView()
+    func configureNavigationBar()
+    func configureHierarchy()
+    func configureUI()
+    func configureGestureAndButtonActions()
+    func configureLayout()
+    
+}
+
+extension BaseViewProtocol {
+    
+    func configureView() {
+        configureNavigationBar()
+        configureHierarchy()
+        configureUI()
+        configureLayout()
+        configureGestureAndButtonActions()
+    }
+
+    func configureNavigationBar() {
+        
+    }
+    
+    func configureGestureAndButtonActions() {
+        
+    }
+    
+}
