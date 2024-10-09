604f22d5d200736bdeb7340d5038fafbddbc08e1
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-10-09 21:59:04 +0900

    feat(CoreData): CoreDataManager 구현
    "좋아요"컨텐츠를 Create, Read, Delete 할 수 있는 기능 구현

---
+++ TongueLaw/CoreData/CoreDataManager.swift
@@ -0,0 +1,45 @@
+//
+//  CoreDataManager.swift
+//  TongueLaw
+//
+//  Created by 김수경 on 10/9/24.
+//
+
+import CoreData
+
+final class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "FavoriteModel")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unresolved error \(error.localizedDescription)")
+            }
+        }
+        return container
+    }()
+    var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    private init() {}
+    
+    func saveContext(backgroundContext: NSManagedObjectContext? = nil) -> Bool {
+        let context = backgroundContext ?? context
+        
+        if context.hasChanges == false { return true }
+        
+        do {
+            try context.save()
+            
+            return true
+        } catch let error as NSError {
+            print("Error: \(error.localizedDescription)")
+            
+            return false
+        }
+    }
+
+}
---
+++ TongueLaw/DataModel/FavoriteContent.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteContent.swift
+//  TongueLaw
+//
+//  Created by 김수경 on 10/9/24.
+//
+
+import Foundation
+
+struct FavoriteContent {
+    let id: String
+    let title: String?
+    let image: Data?
+    
+    init(id: String, title: String?, image: Data?) {
+        self.id = id
+        self.title = title
+        self.image = image
+    }
+    
+    init(entity: FavoriteEntity) {
+        self.id = entity.id
+        self.title = entity.title
+        self.image = entity.image
+    }
+    
+}
